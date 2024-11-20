e07bd2b5c40405ef8e5a58696ccc19a8c1b2f0a1
Mahdi Sabahi <mrmahdisabahi1@gmail.com>
2024-11-20 15:40:48 +0330

    dvaskj

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header></Header>
+      <Header>djvl</Header>
       <Routes>
         <Route path="/" element={<Header />}></Route>
       </Routes>
